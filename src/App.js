36f8981383797ee0cbf90dec3ded1ea11265cafd
Cauane Cardoso <cauhcdz@gmail.com>
2024-10-29 19:59:49 -0300

    refatorando o post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import { useFetch } from './hooks/useFetch';
+
+const url = "http://localhost:3000/products"
 
 function App() {
+  const [products, setProducts] = useState([])
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
+  const { data: items, httpConfig, loading, error } = useFetch(url);
+
+  // useEffect (() => {
+  //   async function fetchData() {
+  //     const res = await fetch (url);
+
+  //     const data = await res.json();
+
+  //     setProducts(data);
+  //   }
+
+  //   fetchData();
+  // }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name, 
+      price,
+    }
+
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(product),
+    // })
+
+    // // carregamento dinâmico
+    // const addedProduct = await res.json ()
+    // setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+    // refatorando post com hook
+
+    httpConfig(product, "POST")
+
+    setName("");
+    setPrice("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de produtos</h1>
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      <ul>
+        {items && items.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
+        ))}
+      </ul>
+      <div className='add-product'>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input 
+              type='text' 
+              value={name} 
+              name='name' 
+              onChange={(e) => setName(e.target.value)}/>
+          </label>
+          <label>
+            Preço:
+            <input 
+              type='number' 
+              value={price} 
+              name='price' 
+              onChange={(e) => setPrice(e.target.value)}/>
+          </label>
+          {loading && <input type='submit' disabled value='Aguarde' /> }
+          {!loading && <input type='submit' value='Criar' /> }
+          
+        </form>
+      </div>
     </div>
   );
 }
